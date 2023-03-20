36e9be60b56d7a11bd40737e6f936a964b96d13f
shanja-glinka <rul3iofu1s7@outlook.com>
2023-03-20 19:03:42 +0300

    A

--- assets/frontend/js/constructor.js
+++ assets/frontend/js/constructor.js
@@ -2,18 +2,24 @@ const liveConstruct = () => {
     liveConstructTemoholder = null;
 
     let color = document.querySelector('input[name="color"]:checked');
+    let type = document.querySelector('select[name="type"]');
+    let date = document.querySelector('input[name="date"]').value;
+    let time = document.querySelector('input[name="time"]:checked');
 
     color = color == null ? 1 : color.value;
+    type = type.selectedIndex ? type.options[type.selectedIndex].value : 'sticker';
+    date = new Date(date);
+    time = time ? date.getTime() : 0;
 
     data = {
         'id': 1,
-        'date': new Date().getTime(),
-        'type': 'sticker',
+        'date': date.getTime(),
+        'type': type,
         'version': '1.00',
         "event": null,
         "color": color,
         'data': {
-            'time': new Date().getTime(),
+            'time': time,
             'text': document.querySelector('input[name="title"]').value.trim(),
             'descr': document.querySelector('textarea[name="description"]').value.trim(),
             'rotate': 15
@@ -36,6 +42,9 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!document.querySelector('input[name="color"]:checked'))
         document.querySelector('input[name="color"]').checked = true;
 
+    if (!document.querySelector('input[name="date"]').value)
+        document.querySelector('input[name="date"]').valueAsDate = new Date();
+
     let eventFunc = (el) => {
         el.addEventListener('change', () => {
             if (liveConstructTemoholder !== null)
@@ -47,6 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.querySelectorAll('.sticker-create input').forEach(eventFunc);
     document.querySelectorAll('.sticker-create textarea').forEach(eventFunc);
+    document.querySelectorAll('.sticker-create select').forEach(eventFunc);
 
 
     liveConstruct();
